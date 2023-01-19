d38cfa56132b31c3631070abac5e5ead3e05a5e2
gilteunchoi <gilteunchoi@gmail.com>
2023-01-19 14:57:06 +0900

    build: Spring Initializr를 통한 프로젝트 생성
    참조 - https://start.spring.io/

---
+++ src/main/java/me/washcar/wcnc/WcncApplication.java
@@ -0,0 +1,13 @@
+package me.washcar.wcnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/washcar/wcnc/WcncApplicationTests.java
@@ -0,0 +1,13 @@
+package me.washcar.wcnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
