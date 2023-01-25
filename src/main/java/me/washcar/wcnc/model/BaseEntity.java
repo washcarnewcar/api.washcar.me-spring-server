03b7cd1611241f4f994b0cdbbb997a7f7f2861b6
gilteunchoi <gilteunchoi@gmail.com>
2023-01-25 18:10:35 +0900

    refactor: Base 와 NamedEntity 의 Getter Setter 구조 변경 및 externalId 추가
    클라이언트에서 서비스 사용량 추측을 어렵게 만들기 위해 외부에 노출될 랜덤 UUID 를 PrePersist 단계에서 생성함. BaseEntity 의 Column 들에는 Setter 제외.

--- src/main/java/me/washcar/wcnc/model/BaseEntity.java
+++ src/main/java/me/washcar/wcnc/model/BaseEntity.java
@@ -1,20 +1,26 @@
 package me.washcar.wcnc.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+
+    @Column(nullable = false)
+    private UUID externalId;
 
     @CreatedDate
     private LocalDateTime createdDate;
@@ -22,16 +28,9 @@ public class BaseEntity implements Serializable {
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public boolean isNew() {
-        return this.id == null;
+    @PrePersist
+    private void onCreate(){
+        this.externalId = UUID.randomUUID();
     }
 
 }
--- src/main/java/me/washcar/wcnc/model/NamedEntity.java
+++ src/main/java/me/washcar/wcnc/model/NamedEntity.java
@@ -2,20 +2,15 @@ package me.washcar.wcnc.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter @Setter
 public class NamedEntity extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String toString() {
