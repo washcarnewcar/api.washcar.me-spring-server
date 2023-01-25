3dfc15223ad45661ec42f41c468a0657c18cfebf
gilteunchoi <gilteunchoi@gmail.com>
2023-01-25 23:19:07 +0900

    feat: SecurityConfiguration 개발용 설정 추가
    개발 편의를 위해 v2 API 전체를 화이트리스트에 추가함

---
+++ src/main/java/me/washcar/wcnc/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package me.washcar.wcnc.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private static final String[] WHITELIST = {
+            "/v2/**" //TODO 배포 전 화이트리스트 재설정 필요
+    };
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+
+        http.authorizeHttpRequests()
+            .requestMatchers(WHITELIST)
+            .permitAll()
+            .anyRequest()
+            .authenticated();
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+}
